65227ac61adb5ff647f2e50c3b4e94d4e13ab6c7
jacob <jacob.mckenzie@icloud.com>
2025-02-20 19:19:49 +0000

    Added more ids

--- daisy-component-project/src/components/Calculator.tsx
+++ daisy-component-project/src/components/Calculator.tsx
@@ -5,22 +5,23 @@ const Calculator: React.FC = () => {
         return (
 
                 <div id="savings-calcuator" className="mx-auto grid grid-rows-3 grid-cols-6 gap-6 w-full max-w-2xl border border-gray-200 rounded-lg p-6 shadow-md">
+
                     {/* Row 1: Label */}
-                    <div id="savings-calculator-label" className="col-span-6 text-center">
+                    <div id="savings-calculator-header" className="col-span-6 text-center">
                         <div className="p-3 text-2xl">Savings Calculator</div>
                         <div className="divider"></div>
                     </div>
 
                     {/* Row 2: Spinner & Input Fields */}
-                    <div className="col-span-6 grid gap-4 grid-cols-6 items-center">
+                    <div  id="savings-calculator-main" className="col-span-6 grid gap-4 grid-cols-6 items-center">  
                         
                         {/* Spinner (Left 3 Columns) */}
-                        <div id="savings-loaded-content" className="col-span-3 flex justify-center items-center">
+                        <div id="savings-calculator-loaded-content" className="col-span-3 flex justify-center items-center">
                         <span className="loading loading-spinner loading-x"></span>
                         </div>
 
-                        {/* Empty Space (Middle 1 Column) */}
-                        <div className="col-span-3 flex flex-col gap-4 items-end">
+                        {/* Labels and Input (Right 3 Columns) */}
+                        <div  id="savings-calculator-buttons" className="col-span-3 flex flex-col gap-4 items-end">
                             <label className="input">
                                 <span className="label">House Value</span>
                                 <input type="text" placeholder="URL" />
@@ -33,7 +34,7 @@ const Calculator: React.FC = () => {
                     </div>
 
                     {/* Row 3: Buttons */}
-                    <div className="col-span-6 grid grid-cols-6 gap-4">
+                    <div id="savings-calculator-buttons" className="col-span-6 grid grid-cols-6 gap-4">
                         <button className="btn btn-primary col-span-3">Calculate</button>
                         <button className="btn btn-secondary col-span-3">Clear</button>
                         <div className="divider col-span-6"></div>
